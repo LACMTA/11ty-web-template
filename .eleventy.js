cb83140a4040d3add3d5d3b212bf34fd4b7f9467
Nina Kin <matikin9@gmail.com>
2022-03-14 15:30:49 -0700

    reorg and added css+js folders

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
 module.exports = function(eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("src/css");
+	eleventyConfig.addPassthroughCopy("src/js");
 
 	return {
 		pathPrefix: "/11ty-web-template/",
 		dir: {
+			input: "src",
 			output: "docs"
 		}
 	}
